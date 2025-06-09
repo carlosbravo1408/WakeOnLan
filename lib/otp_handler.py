27ea3d652403b9fcaaef357da9e3929c0ae261b6
carlos <carlos.j.bravo@hotmail.com>
2025-06-09 02:25:35 -0500

    feat: Implement OTPHandler for OTP validation, generation, and QR creation
    The OTPHandler class has been created to manage OTP validations, generate
    new OTPs when required, and create QR codes in a format readable by
    Authenticator Apps.

---
+++ lib/otp_handler.py
@@ -0,0 +1,27 @@
+import pyotp
+
+
+class OtpHandler:
+    def __init__(self, secret, name,  issuer_name="AlienNerd Dev"):
+        self._name = name
+        self._issuer_name = issuer_name
+        self._totp = pyotp.TOTP(secret)
+
+    def get_uri(self):
+        return self._totp.provisioning_uri(
+            name=self._name,
+            issuer_name=self._issuer_name
+        )
+
+    def generate_qr(self):
+        try:
+            from qrcode.main import QRCode
+            qr = QRCode(version=1, box_size=10, border=4)
+            qr.add_data(self.get_uri())
+            qr.make(fit=True)
+            return qr.make_image()
+        except ModuleNotFoundError:
+            return None
+
+    def verify(self, secret: str) -> bool:
+        return self._totp.verify(secret)
